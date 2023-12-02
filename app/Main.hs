3140b815b1138a91c756529935ab861ae7e4d294
knyhago <knyhago@outlook.com>
2023-12-02 23:44:25 +0000

    2 stable

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,5 @@
-module Main where
-
-import Data.Text (Text, unpack)  -- Import Text and unpack function from Data.Text
-import qualified Data.Text as T  -- Qualified import for Text
-
+import Data.Text (Text, unpack)
+import qualified Data.Text as T
 import Fetch (fetchData)
 import Parse (parseMovieTitles)
 import Database
@@ -15,24 +12,40 @@ main = do
         Nothing -> putStrLn "Failed to fetch data."
         Just response -> do
             putStrLn "Data fetched!"
-            
+
             putStrLn "Parsing movie titles..."
             let movieTitles = parseMovieTitles response
             putStrLn "Parsed Movie Titles:"
             mapM_ (putStrLn . unpackText) movieTitles
-            
-            putStrLn "Connecting to the database..."
-            withConn "movies.db" $ \conn -> do
-                putStrLn "Creating the 'movies' table..."
-                createTable conn
 
-                putStrLn "Inserting movies into the database..."
-                let movies = [ MovieDB 1 (T.pack "Movie Title") 2023 ] -- Replace with actual movie data
-                mapM_ (\movie -> insertMovie conn movie) movies
+            putStrLn "Choose an operation:"
+            putStrLn "1. Insert movies into the database"
+            putStrLn "2. Fetch movies from the database"
+            putStrLn "3. Exit"
 
-                putStrLn "Fetching movies from the database..."
-                storedMovies <- getMovies conn
-                print storedMovies
+            choice <- getLine
+            case choice of
+                "1" -> do
+                    putStrLn "Inserting movies into the database..."
+                    -- Logic to insert movies into the database
+                    putStrLn "Fetching movies from the database..."
+                    putStrLn "Connecting to the database..."
+                    withConn "movies.db" $ \conn -> do
+                        putStrLn "Inserting movies into the database..."
+                        let movies = [ MovieDB 1 (T.pack "Movie Title") 2023 ] -- Replace with actual movie data
+                        mapM_ (\movie -> insertMovie conn movie) movies
+
+                "2" -> do
+                    putStrLn "Fetching movies from the database..."
+                    putStrLn "Connecting to the database..."
+                    withConn "movies.db" $ \conn -> do
+                    
+                    -- Logic to fetch movies from the database
+                        putStrLn "Fetching movies from the database..."
+                        storedMovies <- getMovies conn
+                        print storedMovies
+                "3" -> putStrLn "Exiting."
+                _ -> putStrLn "Invalid choice. Please choose again."
 
 -- Function to convert Text to String
 unpackText :: Text -> String
